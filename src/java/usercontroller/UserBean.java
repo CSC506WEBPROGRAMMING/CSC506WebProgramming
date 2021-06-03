732b791547c275447a953f34eb15b12f918d673a
uyauya16 <samueluyauya16@gmail.com>
2021-06-03 15:37:07 +0100

    Getters and Setters of the declared private variables are created.. This will help in accessing The DAO (Data Access Object)class which retrieving data from database

--- src/java/usercontroller/UserBean.java
+++ src/java/usercontroller/UserBean.java
@@ -16,4 +16,66 @@ public class UserBean {
       public boolean valid;
 
 
+       public String getErrorMessage() {
+	          return errorMessage;
+	      }
+
+	      public String getName() {
+	          return name;
+	      }
+
+	      public void setName(String name) {
+	          this.name = name;
+	      }
+
+	      public String getPhone() {
+	          return phone;
+	      }
+
+	      public void setPhone(String phone) {
+	          this.phone = phone;
+	      }
+
+	      public String getAddress() {
+	          return address;
+	      }
+
+	      public void setAddress(String address) {
+	          this.address = address;
+	      }
+
+	      public void setErrorMessage(String errorMessage) {
+	          this.errorMessage = errorMessage;
+	      }
+
+
+
+
+	        public String getPassword() {
+	           return password;
+	  	}
+
+	        public void setPassword(String newPassword) {
+	           password = newPassword;
+	  	}
+
+
+	        public String getUsername() {
+	           return username;
+	  			}
+
+	        public void setUserName(String newUsername) {
+	           username = newUsername;
+	  			}
+
+
+	        public boolean isValid() {
+	           return valid;
+	  	}
+
+	        public void setValid(boolean newValid) {
+	           valid = newValid;
+	}
+
+
 }
