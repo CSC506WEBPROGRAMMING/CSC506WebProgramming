78b4b0e89eed7af421465f3acf5a8e95845a4376
Yunusa3294 <83475821+Yunusa3294@users.noreply.github.com>
2021-06-03 17:05:34 +0100

    Login Servlet file is created which is the login controller. it validate a user login.

---
+++ src/java/usercontroller/LoginServlet.java
@@ -0,0 +1,15 @@
+package usercontroller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+public class LoginServlet extends HttpServlet {
+
+
+}
+
