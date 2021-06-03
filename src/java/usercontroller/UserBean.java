5284e1238afcc9e3e63eadc8f1bb6a2abc374048
uyauya16 <samueluyauya16@gmail.com>
2021-06-03 14:54:58 +0100

    A UserBean class is been declared, the veriables for user login, that is the name,email,phone,address, phone, and password

---
+++ src/java/usercontroller/UserBean.java
@@ -0,0 +1,19 @@
+
+
+package usercontroller;
+
+public class UserBean {
+
+      private String username;
+      private String password;
+      private String name;
+
+      private String phone;
+      private String address;
+
+      private String errorMessage;
+
+      public boolean valid;
+
+
+}
