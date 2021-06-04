411ab37722825e6a35ac4812c3564995bffc5361
csc15u3255 <ishakuj36@gmail.com>
2021-06-04 19:50:14 +0100

    some codes is been added to perform try and catch exception, it verify the user input

--- src/java/usercontroller/ProfileUpdate.java
+++ src/java/usercontroller/ProfileUpdate.java
@@ -27,6 +27,73 @@ public class ProfileUpdate extends HttpServlet {
      
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
+// gets values of text fields
+        
+       
+        String name = request.getParameter("name");
+        String phone = request.getParameter("phone");
+         String address = request.getParameter("address");
+         
+        InputStream inputStream = null; // input stream of the upload file
+         
+        
+        // obtains the upload file part in this multipart request
+        Part filePart = request.getPart("image");
+        if (filePart != null) {
+            // prints out some information for debugging
+            System.out.println(filePart.getName());
+            System.out.println(filePart.getSize());
+            System.out.println(filePart.getContentType());
+             
+            // obtains input stream of the upload file
+            inputStream = filePart.getInputStream();
+        }
+         
+        Connection conn = null; // connection to the database
+        String message = null;  // message will be sent back to client
+         
+        try {
+            // connects to the database
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
+ 
+            // constructs SQL statement
+            String sql = "INSERT INTO `csc506web`.`register` ( `name`, `phone`, `address`, `image` ) VALUES ( ?, ?, ?, ?)  ON DUPLICATE KEY UPDATE `name` = VALUES (name), `phone` = VALUES (phone), `address` = VALUES (address), `image` = VALUES (image);";
+            PreparedStatement statement = conn.prepareStatement(sql);
+           
+            statement.setString(1, name);
+            statement.setString(2, phone);
+             statement.setString(3, address);
+             
+            if (inputStream != null) {
+                // fetches input stream of the upload file for the blob column
+                statement.setBlob(4, inputStream);
+            }
+ 
+            // sends the statement to the database server
+            int row = statement.executeUpdate();
+            if (row > 0) {
+                message = "Profile updated";
+      
+            }
+        } catch (SQLException ex) {
+            message = "ERROR: " + ex.getMessage();
+            ex.printStackTrace();
+        } finally {
+            if (conn != null) {
+                // closes the database connection
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            // sets the message in request scope
+            request.setAttribute("Message", message);
+             
+            // forwards to the message page
+            getServletContext().getRequestDispatcher("/profile.jsp").forward(request, response);
+        }
  }
         
 }
