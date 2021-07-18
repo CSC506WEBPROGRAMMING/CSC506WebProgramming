6ec94e2f0a84dcd059aaad055e11a9660a1c60f1
DlamaVandi <84595820+DlamaVandi@users.noreply.github.com>
2021-07-18 05:21:11 -0700

    Dumping data for table comment

--- csc506web.sql
+++ csc506web.sql
@@ -32,6 +32,9 @@ CREATE TABLE IF NOT EXISTS `comment` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
+--
+-- Dumping data for table `comment`
+--
 
 --
 -- Table structure for table `register`
